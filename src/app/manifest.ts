b1db7fc5f7d95239f556ec63a298a32de1a68836
Alberto <1123alberto@users.noreply.github.com>
2026-06-21 15:18:29 +0300

    Add stable ID to PWA manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,6 +2,7 @@ import type { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
+    id: '/',
     name: 'MySuper.gr | Σύγκριση Τιμών Σούπερ Μάρκετ',
     short_name: 'MySuper.gr',
     description: 'Βρείτε τις καλύτερες τιμές στα ελληνικά σούπερ μάρκετ. Υπολογίστε το φθηνότερο καλάθι αγορών ή βελτιστοποιήστε τις αγορές σας σε πολλά καταστήματα.',
